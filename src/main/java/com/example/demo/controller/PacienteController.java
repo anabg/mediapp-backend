ba08aa00b38d0d7b0706221fccbb95a768b13009
Grimaut, Ana <Ana_Grimaut@McAfee.com>
2018-09-26 11:38:26 -0300

    agrego nuevos metodos en el controller

--- src/main/java/com/example/demo/controller/PacienteController.java
+++ src/main/java/com/example/demo/controller/PacienteController.java
@@ -10,15 +10,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.demo.service.IPacienteService;
+import com.example.demo.exception.ModeloNotFoundException;
 import com.example.demo.model.Paciente;
 
 @RestController
@@ -28,6 +31,10 @@ public class PacienteController {
 	@Autowired
 	private IPacienteService pacienteService;
 
+	/**
+	 * 
+	 * @return
+	 */
 	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<List<Paciente>> listar() {
 		List<Paciente> pacientes = new ArrayList<>();
@@ -35,21 +42,62 @@ public class PacienteController {
 		return new ResponseEntity<List<Paciente>>(pacientes, HttpStatus.OK);
 	}
 
+	/**
+	 * 
+	 * @param id
+	 * @return
+	 */
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Paciente> listarId(@PathVariable("id") Integer id) {
 		Paciente paciente = pacienteService.listar(id);
 		return new ResponseEntity<Paciente>(paciente, HttpStatus.OK);
 	}
-	
+
+	/**
+	 * 
+	 * @param paciente
+	 * @return
+	 */
 	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<Object> registrar(@Valid @RequestBody Paciente paciente){
-		
+	public ResponseEntity<Object> registrar(@Valid @RequestBody Paciente paciente) {
+
+		/**
+		 * { "nombres":"ana belen" , "apellidos": "grimaut", "dni":"12345678",
+		 * "direccion":"algo", "telefono":"123456789"
+		 * 
+		 * }
+		 */
 		Paciente pac = new Paciente();
 		pac = pacienteService.registrar(paciente);
-		
-		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/id").buildAndExpand(pac.getIdPaciente()).toUri();
+
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/id").buildAndExpand(pac.getIdPaciente())
+				.toUri();
 		return ResponseEntity.created(location).build();
-				
+
+	}
+
+	/**
+	 * 
+	 * @param paciente
+	 * @return
+	 */
+	@PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Object> actualizar(@Valid @RequestBody Paciente paciente) {
+		pacienteService.modificar(paciente);
+		return new ResponseEntity<Object>(HttpStatus.OK);
+	}
+
+	@DeleteMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public void eliminar(@PathVariable Integer id) {
+		Paciente paciente = pacienteService.listar(id);
 		
+		if(paciente!=null) {
+			
+			throw new ModeloNotFoundException("ID:" + id);
+			
+		}
+		else {
+			pacienteService.eliminar(id);
+		}
 	}
 }
--- src/main/java/com/example/demo/service/impl/PacienteServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/PacienteServiceImpl.java
@@ -3,11 +3,13 @@ package com.example.demo.service.impl;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.example.demo.dao.IPacienteDAO;
 import com.example.demo.model.Paciente;
 import com.example.demo.service.IPacienteService;
 
+@Service
 public class PacienteServiceImpl implements IPacienteService {
 
 	@Autowired
